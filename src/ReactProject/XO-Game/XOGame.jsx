1b56d5ca63b1e5af3aa9b1661c517c6f2e4172ce
Raji <rajeswarim0112@gmail.com>
2025-01-10 16:54:05 +0530

    update XOGame file

--- src/ReactProject/XO-Game/XOGame.jsx
+++ src/ReactProject/XO-Game/XOGame.jsx
@@ -1,3 +1,64 @@
+// import React, { useState } from "react";
+// import { Win } from "./Win";
+// import Board from "./Board";
+// import Nav from "../Nav";
+
+// const style = {
+//   container: {
+//     display: "flex",
+//     flexDirection: "column",
+//     alignContent: "center",
+//     justifyContent: "space-evenly",
+//     height: "100%",
+//     marginTop: "50px"
+//   },
+//   info: {
+//     fontSize: "2rem",
+//     opacity: "0.5",
+    
+//   },
+//   btn: {
+//     fontSize:"20px", 
+//     marginTop:"30px",
+//     marginLeft:"700px",
+//   }
+// };
+
+// const XOGame = () => {
+//   const [board, setBoard] = useState(Array(9).fill(null));
+//   const [xTurn, setXTurn] = useState(true);
+//   const winner = Win(board);
+
+//   const handleClick = (i) => {
+//     const tmpBoard = [...board];
+//     if (!!winner || !!tmpBoard[i]) return;
+
+//     tmpBoard[i] = xTurn ? "X" : "O";
+//     setBoard(tmpBoard);
+//     setXTurn(!xTurn);
+//   };
+
+//   const resetBoard = () => (
+//     <button style={style.btn}>Start Game</button>
+//   );
+
+//   return (
+//     <>
+//     <Nav/>
+//     <div style={style.container}>
+//       <p style={style.info}>
+//         {winner ? "Winner: " + winner : "Next Player: " + (xTurn ? "X" : "O")}
+//       </p>
+//       <Board squares={board} handleClick={handleClick} />
+//       <div>{resetBoard()}</div>
+//     </div>
+//     </>
+//   );
+// };
+
+// export default XOGame;
+
+
 import React, { useState } from "react";
 import { Win } from "./Win";
 import Board from "./Board";
@@ -15,12 +76,11 @@ const style = {
   info: {
     fontSize: "2rem",
     opacity: "0.5",
-    
   },
   btn: {
-    fontSize:"20px", 
-    marginTop:"30px",
-    marginLeft:"700px",
+    fontSize: "20px",
+    marginTop: "30px",
+    marginLeft: "700px",
   }
 };
 
@@ -38,22 +98,26 @@ const XOGame = () => {
     setXTurn(!xTurn);
   };
 
-  const resetBoard = () => (
-    <button style={style.btn}>Start Game</button>
-  );
+  const resetBoard = () => {
+    setBoard(Array(9).fill(null));
+    setXTurn(true);
+  };
 
   return (
     <>
-    <Nav/>
-    <div style={style.container}>
-      <p style={style.info}>
-        {winner ? "Winner: " + winner : "Next Player: " + (xTurn ? "X" : "O")}
-      </p>
-      <Board squares={board} handleClick={handleClick} />
-      <div>{resetBoard()}</div>
-    </div>
+      <Nav />
+      <div style={style.container}>
+        <p style={style.info}>
+          {winner ? "Winner: " + winner : "Next Player: " + (xTurn ? "X" : "O")}
+        </p>
+        <Board squares={board} handleClick={handleClick} />
+        <button style={style.btn} onClick={resetBoard}>
+          Start Game
+        </button>
+      </div>
     </>
   );
 };
 
 export default XOGame;
+
