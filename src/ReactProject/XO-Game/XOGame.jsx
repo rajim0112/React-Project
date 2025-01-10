8dbe3b064dd8618c2ac54efedf6dc022f82a7c91
Raji <rajeswarim0112@gmail.com>
2025-01-10 17:03:06 +0530

    update XOGame

--- src/ReactProject/XO-Game/XOGame.jsx
+++ src/ReactProject/XO-Game/XOGame.jsx
@@ -1,64 +1,3 @@
-// import React, { useState } from "react";
-// import { Win } from "./Win";
-// import Board from "./Board";
-// import Nav from "../Nav";
-
-// const style = {
-//   container: {
-//     display: "flex",
-//     flexDirection: "column",
-//     alignContent: "center",
-//     justifyContent: "space-evenly",
-//     height: "100%",
-//     marginTop: "50px"
-//   },
-//   info: {
-//     fontSize: "2rem",
-//     opacity: "0.5",
-    
-//   },
-//   btn: {
-//     fontSize:"20px", 
-//     marginTop:"30px",
-//     marginLeft:"700px",
-//   }
-// };
-
-// const XOGame = () => {
-//   const [board, setBoard] = useState(Array(9).fill(null));
-//   const [xTurn, setXTurn] = useState(true);
-//   const winner = Win(board);
-
-//   const handleClick = (i) => {
-//     const tmpBoard = [...board];
-//     if (!!winner || !!tmpBoard[i]) return;
-
-//     tmpBoard[i] = xTurn ? "X" : "O";
-//     setBoard(tmpBoard);
-//     setXTurn(!xTurn);
-//   };
-
-//   const resetBoard = () => (
-//     <button style={style.btn}>Start Game</button>
-//   );
-
-//   return (
-//     <>
-//     <Nav/>
-//     <div style={style.container}>
-//       <p style={style.info}>
-//         {winner ? "Winner: " + winner : "Next Player: " + (xTurn ? "X" : "O")}
-//       </p>
-//       <Board squares={board} handleClick={handleClick} />
-//       <div>{resetBoard()}</div>
-//     </div>
-//     </>
-//   );
-// };
-
-// export default XOGame;
-
-
 import React, { useState } from "react";
 import { Win } from "./Win";
 import Board from "./Board";
@@ -76,12 +15,16 @@ const style = {
   info: {
     fontSize: "2rem",
     opacity: "0.5",
+    
   },
   btn: {
     fontSize: "20px",
     marginTop: "30px",
-    marginLeft: "700px",
+    padding: "10px 20px", 
+    alignSelf: "center",  
+    width: "150px",       
   }
+  
 };
 
 const XOGame = () => {
@@ -105,19 +48,16 @@ const XOGame = () => {
 
   return (
     <>
-      <Nav />
-      <div style={style.container}>
-        <p style={style.info}>
-          {winner ? "Winner: " + winner : "Next Player: " + (xTurn ? "X" : "O")}
-        </p>
-        <Board squares={board} handleClick={handleClick} />
-        <button style={style.btn} onClick={resetBoard}>
-          Start Game
-        </button>
-      </div>
+    <Nav/>
+    <div style={style.container}>
+      <p style={style.info}>
+        {winner ? "Winner: " + winner : "Next Player: " + (xTurn ? "X" : "O")}
+      </p>
+      <Board squares={board} handleClick={handleClick} />
+      <button style={style.btn} onClick={resetBoard}>Start Game</button>
+    </div>
     </>
   );
 };
 
 export default XOGame;
-
